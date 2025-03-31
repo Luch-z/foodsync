f8b8a58e25107ab2d37549b093b73b0afba86e09
Luch-z <ricardopacja@gmail.com>
2025-03-31 19:21:12 -0300

    link to redirect to the login page

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-    return <div>
-        <div className="text-center pt-3">
-            <h1 className="text-3xl">FoodSynch</h1>
+    return (
+        <div>
+            <div className="text-center pt-3">
+                <h1 className="text-3xl">FoodSynch</h1>
+                <Link href={"/login"}>
+                    <Button className="mt-3">Login</Button>
+                </Link>
+            </div>
         </div>
-    </div>;
+    );
 }
